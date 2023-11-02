d445fe8c32d0e7ca9301f4407d9d2e099418d6d2
Li Yong Pan <michelpan@hotmail.com>
2023-11-03 00:32:27 +0100

    simple navbar as taskbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,13 @@
 "use client";
 
 export default function Home() {
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="intro flex-row">
-        <h1 className="text-[100px] font-semibold text-right transition-opacity">
-          ✌🏻
-          <br />I am
-          <p>
-            Michel
-          </p>
-        </h1>
+    <div className="fixed bottom-0 left-0 w-full h-16 bg-gray-100 text-black flex items-center justify-center">
+      <div className="flex space-x-4">
+        <button className="hover:bg-gray-300 px-4 py-2 rounded">About</button>
+        <button className="hover:bg-gray-300 px-4 py-2 rounded">Projects</button>
+        <button className="hover:bg-gray-300 px-4 py-2 rounded">Contact</button>
       </div>
-    </main>
-    
+    </div>
   );
 }
