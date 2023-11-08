2f5a66a66e4ab112511c9e561337634025e77ec3
Li Yong Pan <michelpan@hotmail.com>
2023-11-09 00:57:54 +0100

    refactor and add intro

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,13 @@ import Taskbar from "@/components/TaskBar";
 export default function Home() {
   return (
     <>
-      <Taskbar />
       <MenuBar />
-      <div class="min-h-screen flex flex-col items-center justify-center">
-        <p class="text-9xl text-center font-semibold">Hi, I am Michel Pan</p>
+
+      <div class="min-h-screen flex flex-col items-center justify-center text-center">
+        <p class="font-semibold text-4xl">Hi, I am Michel Pan</p>
       </div>
+
+      <Taskbar />
     </>
   );
 }
