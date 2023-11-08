97b5bf2a9f47a363a8c2bb68818a083de8df84bc
Li Yong Pan <michelpan@hotmail.com>
2023-11-08 22:10:14 +0100

    refactor taskbar items

--- src/components/TaskBar.js
+++ src/components/TaskBar.js
@@ -1,21 +1,12 @@
+import TaskBarItem from "./TaskBarIcon";
+
 export default function Taskbar() {
   return (
     <div className="fixed bottom-0 left-0 w-full h-16 bg-gray-100 text-black flex items-center justify-center">
       <div className="flex space-x-4">
-        <div className="flex flex-col items-center hover:bg-gray-300 px-2 py-2 rounded">
-          <img src="./icon.png" alt="Logo" className="h-8 w-8" />
-          <span className="text-xs">About</span>
-        </div>
-
-        <div className="flex flex-col items-center hover:bg-gray-300 px-2 py-2 rounded">
-          <img src="./icon.png" alt="Logo" className="h-8 w-8" />
-          <span className="text-xs">Projects</span>
-        </div>
-
-        <div className="flex flex-col items-center hover:bg-gray-300 px-2 py-2 rounded">
-          <img src="./icon.png" alt="Logo" className="h-8 w-8" />
-          <span className="text-xs">Contact</span>
-        </div>
+        <TaskBarItem name="About" />
+        <TaskBarItem name="Projects" />
+        <TaskBarItem name="Contact" />
       </div>
     </div>
   );
---
+++ src/components/TaskBarIcon.js
@@ -0,0 +1,8 @@
+export default function TaskBarItem({ name = "WorksThis" }) {
+  return (
+    <div className="flex flex-col items-center hover:bg-gray-300 px-2 py-2 rounded">
+      <img src="./icon.png" alt="Logo" className="h-8 w-8" />
+      <span className="text-xs">{name}</span>
+    </div>
+  );
+}
