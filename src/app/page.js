5033e53d88893ca12dbb81abc8a5b66a1de45725
Li Yong Pan <michelpan@hotmail.com>
2023-11-08 21:44:54 +0100

    add icon to taskbar items

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,20 @@ export default function Home() {
   return (
     <div className="fixed bottom-0 left-0 w-full h-16 bg-gray-100 text-black flex items-center justify-center">
       <div className="flex space-x-4">
-        <button className="hover:bg-gray-300 px-4 py-2 rounded">About</button>
-        <button className="hover:bg-gray-300 px-4 py-2 rounded">Projects</button>
-        <button className="hover:bg-gray-300 px-4 py-2 rounded">Contact</button>
+        <div className="flex flex-col items-center hover:bg-gray-300 px-2 py-2 rounded">
+          <img src="./icon.png" alt="Logo" className="h-8 w-8" />
+          <span className="text-xs">About</span>
+        </div>
+
+        <div className="flex flex-col items-center hover:bg-gray-300 px-2 py-2 rounded">
+          <img src="./icon.png" alt="Logo" className="h-8 w-8" />
+          <span className="text-xs">Projects</span>
+        </div>
+
+        <div className="flex flex-col items-center hover:bg-gray-300 px-2 py-2 rounded">
+          <img src="./icon.png" alt="Logo" className="h-8 w-8" />
+          <span className="text-xs">Contact</span>
+        </div>
       </div>
     </div>
   );
