25d61335d1c759242d7b0f2192b103e48f40d893
Li Yong Pan <michelpan@hotmail.com>
2023-11-09 00:39:45 +0100

    intro

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,9 @@ export default function Home() {
     <>
       <Taskbar />
       <MenuBar />
+      <div class="min-h-screen flex flex-col items-center justify-center">
+        <p class="text-9xl text-center font-semibold">Hi, I am Michel Pan</p>
+      </div>
     </>
   );
 }
