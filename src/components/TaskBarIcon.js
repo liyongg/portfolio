167bddcbdb52a2789c6d57c9b193386a299794c4
Li Yong Pan <michelpan@hotmail.com>
2023-11-21 00:24:02 +0100

    change taskbar icons

--- src/components/TaskBarIcon.js
+++ src/components/TaskBarIcon.js
@@ -1,7 +1,14 @@
-export default function TaskBarItem({ name = "WorksThis" }) {
+import Image from "next/image";
+
+export default function TaskBarItem({ name }) {
   return (
     <div className="flex flex-col items-center hover:bg-gray-300 px-2 py-2 rounded">
-      <img src="/icon.svg" alt="Logo" className="h-8 w-8" />
+      <Image
+        src={`/${name.toLowerCase()}.svg`}
+        width="32"
+        height="32"
+        alt="Logo"
+      />
       <span className="text-xs">{name}</span>
     </div>
   );
